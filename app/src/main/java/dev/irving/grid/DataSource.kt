b067c6ec5f4042298760b3740f7d7fc4ad33587f
Irving Lop <irving.dev.mobi@gmail.com>
2023-06-22 12:33:23 -0600

    U3 -> P2 -> 4 Practice: Build a grid

---
+++ app/src/main/java/dev/irving/grid/DataSource.kt
@@ -0,0 +1,20 @@
+package dev.irving.grid
+
+object DataSource {
+    val topics = listOf(
+        TopicUiModel(R.string.architecture, 58, R.drawable.architecture),
+        TopicUiModel(R.string.crafts, 121, R.drawable.crafts),
+        TopicUiModel(R.string.business, 78, R.drawable.business),
+        TopicUiModel(R.string.culinary, 118, R.drawable.culinary),
+        TopicUiModel(R.string.design, 423, R.drawable.design),
+        TopicUiModel(R.string.fashion, 92, R.drawable.fashion),
+        TopicUiModel(R.string.film, 165, R.drawable.film),
+        TopicUiModel(R.string.gaming, 164, R.drawable.gaming),
+        TopicUiModel(R.string.drawing, 326, R.drawable.drawing),
+        TopicUiModel(R.string.lifestyle, 305, R.drawable.lifestyle),
+        TopicUiModel(R.string.music, 212, R.drawable.music),
+        TopicUiModel(R.string.painting, 172, R.drawable.painting),
+        TopicUiModel(R.string.photography, 321, R.drawable.photography),
+        TopicUiModel(R.string.tech, 118, R.drawable.tech)
+    )
+}
--- app/src/main/java/dev/irving/grid/MainActivity.kt
+++ app/src/main/java/dev/irving/grid/MainActivity.kt
@@ -3,13 +3,33 @@ package dev.irving.grid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.irving.grid.ui.theme.GridTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +42,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LazyVerticalGrid(
+                        columns = GridCells.Adaptive(180.dp),
+                        contentPadding = PaddingValues(8.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        items(DataSource.topics) { topic ->
+                            TopicView(
+                                topic = topic,
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -30,17 +61,64 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun TopicView(
+    topic: TopicUiModel,
+    modifier: Modifier = Modifier
+) {
+    Card(
         modifier = modifier
-    )
+    ) {
+        Row(
+            verticalAlignment = Alignment.Bottom
+        ) {
+            Image(
+                painter = painterResource(id = topic.imageResource),
+                contentDescription = stringResource(topic.nameResource),
+                modifier = Modifier.size(68.dp)
+            )
+            Column(
+                modifier = Modifier.padding(horizontal = 16.dp)
+            ) {
+                Text(
+                    text = stringResource(topic.nameResource),
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.padding(bottom = 8.dp, top = 16.dp)
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.size(8.dp))
+                    Text(
+                        text = topic.associatedCourses.toString(),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TopicViewPreview() {
     GridTheme {
-        Greeting("Android")
+        val topic = TopicUiModel(
+            R.string.photography,
+            133,
+            R.drawable.photography
+        )
+        TopicView(topic = topic)
     }
 }
+
+data class TopicUiModel(
+    @StringRes val nameResource: Int,
+    val associatedCourses: Int,
+    @DrawableRes val imageResource: Int,
+)
